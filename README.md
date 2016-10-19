708eeff9db0e0d3d93ab1c6ae0273ced42e66728
Shoestrong <zqc_2007@yeah.net>
2016-10-20 00:24:27 +0800

    like demo

--- README.md
+++ README.md
@@ -2,14 +2,26 @@
 
 ***
 
+**B**
+* [ball_pool_game => 模拟现实重力碰撞效果](http://eg.shoestrong.cc/eg/ball_pool_game/) 
+
+
 **C**
 * [calendar_chorme => 仿谷歌日历](http://eg.shoestrong.cc/eg/calendar_chorme/) 
+* [canvas_beautiful_clock => 漂亮的钟表](http://eg.shoestrong.cc/eg/canvas_beautiful_clock/) 
 * [canvas_dot => 例子动画](http://eg.shoestrong.cc/eg/canvas_dot/)
+* [canvas_huitailang => 灰太狼](http://eg.shoestrong.cc/eg/canvas_huitailang/)
+* [change_size_move => 可拖动可拉伸改变大小矩形](http://eg.shoestrong.cc/eg/change_size_move/)
+* [chinese_chess => 中国象棋](http://eg.shoestrong.cc/eg/chinese_chess/)
 * [click_everywhere_heart => 点击页面任意位置显示出心形](http://eg.shoestrong.cc/eg/click_everywhere_heart/)
+* [cloud_letter_ball => 球状文字分布](http://eg.shoestrong.cc/eg/cloud_letter_ball/)
 * [color_grey => 灰度颜色](http://eg.shoestrong.cc/eg/color_grey/)
 
 
 **D**
+* [date_mdater_zepto => 移动端日历](http://eg.shoestrong.cc/eg/date_mdater_zepto/)
+* [date_mtimer_zepto => 移动端时间](http://eg.shoestrong.cc/eg/date_mtimer_zepto/)
+* [drag_list => 拖拽列表效果](http://eg.shoestrong.cc/eg/drag_list/)
 * [draw_by_mouse => 鼠标绘画](http://eg.shoestrong.cc/eg/draw_by_mouse/)
 
 
@@ -20,19 +32,23 @@
 
 
 **G**
-* [get_random_color => 随机改变颜色色值](http://eg.shoestrong.cc/eg/get_random_color/)
 * [get_image_color => 获取图片主要颜色](http://eg.shoestrong.cc/eg/get_image_color/)
+* [get_random_color => 随机改变颜色色值](http://eg.shoestrong.cc/eg/get_random_color/)
+* [greedy_snake_game => 贪吃蛇游戏](http://eg.shoestrong.cc/eg/greedy_snake_game/)
 * [gyroscope_360_rotate_mobile => 陀螺仪移动设备360度旋转](http://eg.shoestrong.cc/eg/gyroscope_360_rotate_mobile/)
 
 
 
 **H**
 * [history_sort => 历史纪录排序](http://eg.shoestrong.cc/eg/history_sort/)
+* [hover_everywhere_lieke_baidu => 模仿百度四边划入](http://eg.shoestrong.cc/eg/hover_everywhere_lieke_baidu/)
 
 
 
 **I**
+* [image_3d_hover => 3D图片划过](http://eg.shoestrong.cc/eg/image_3d_hover/)
 * [image_mirror => 图片镜像](http://eg.shoestrong.cc/eg/image_mirror/)
+* [image_area_select => 裁剪生成头像](http://eg.shoestrong.cc/eg/image_area_select/)
 
 
 
@@ -43,10 +59,12 @@
 
 **L**
 * [load_img => 页面滚动延迟加载图片](http://eg.shoestrong.cc/eg/load_img/)
+* [love_give_friend => 给最爱人的一份表达](http://eg.shoestrong.cc/eg/love_give_friend/)
 
 
 
 **M**
+* [model_3d_rotate => 3D模特展示](http://eg.shoestrong.cc/eg/model_3d_rotate/)
 * [more_text => 中文假文生成器](http://eg.shoestrong.cc/eg/more_text/)
 * [mouse_point => 鼠标跟随效果](http://eg.shoestrong.cc/eg/mouse_point/)
 
@@ -63,16 +81,26 @@
 
 
 **P**
+* [page_switch => 实现图片翻转切换效果](http://eg.shoestrong.cc/eg/page_switch/)
 * [parabola_paowuxian => 抛物线](http://eg.shoestrong.cc/eg/parabola_paowuxian/)
-* [plaxify_guthub404 模仿github404鼠标移动视差效果](http://eg.shoestrong.cc/eg/plaxify_guthub404/) =>
+* [plaxify_guthub404 => 模仿github404鼠标移动视差效果](http://eg.shoestrong.cc/eg/plaxify_guthub404/)
+* [prize_cut => 刮奖](http://eg.shoestrong.cc/eg/prize_cut/)
 
 
+
+**Q**
+* [qietu_scener_fullscreen => 切图全屏](http://eg.shoestrong.cc/eg/qietu_scener_fullscreen/)
+* [qrcode_create => 生成二维码](http://eg.shoestrong.cc/eg/qrcode_create/)
 
 **R**
 * [remove_num_point => 是否为正整数](http://eg.shoestrong.cc/eg/remove_num_point/)
 * [roll_jiang => 转盘抽奖](http://eg.shoestrong.cc/eg/roll_jiang/)
 * [russian_box => 俄罗斯方块游戏](http://eg.shoestrong.cc/eg/russian_box/)
 
+
+
+**S**
+* [select_city_3grade => 三级城市联动](http://eg.shoestrong.cc/eg/select_city_3grade/)
 
 
 **U**
---
+++ eg/drag_list/JScript2.js
@@ -0,0 +1,188 @@
+﻿
+(function (window) {
+    var regId = /^#[\w\-]+$/,
+    regCls = /^.[\w\-]+$/,
+    p_silice = Array.prototype.slice,
+    p_push = Array.prototype.push,
+    markArr = function (obj) {
+        return p_silice.call(obj, 0);
+    };
+    function MWood(selector) {
+        return new MWood.prototype.init(selector);
+    };
+
+    function MWood(selector, context) {
+        return new MWood.prototype.init(selector, context);
+    }
+
+    MWood.prototype = {
+        init: function (selector) {
+            if (regId.test(selector)) {
+                this.length = 1;
+                this[0] = document.getElementById(selector);
+                return this;
+            }
+            else if (regCls.test(selector)) {
+                if (document.querySelectorAll) {
+                    p_push.apply(this, markArr(document.querySelectorAll(selector)));
+                } else {
+                    var len = documenet.getElementsByTagName('*').length,
+                    clsArr = [],
+                    el, i = 0,
+                    selector = selector.replace(/\-/g, "\\-");
+                    var oRegExp = new RegExp("(^|\\s)" + selector + "(\\s|$)");
+
+                    for (; i < len; i++) {
+                        el = els[i];
+                        if (oRegExp.test(el[type])) {
+                            clsArr.push(el);
+                        }
+                    }
+
+                    pro_push.apply(this, markArr(clsArr));
+                    return this;
+                }
+            }
+        },
+        length: 0
+    }
+
+    MWood.prototype.init.prototype = MWood.prototype;
+
+    MWood.extend = function (obj) {
+        for (var o in obj) {
+            this[o] = obj[o];
+        }
+    }
+
+    MWood.extend({
+        addHandle: function (elm, type, fn) {
+            if (elm.addEventListener) {
+                elm.addEventListener(type, fn, false);
+            }
+            else if (el.attachEvent) {
+                elm.attachEvent("on" + type, fn);
+            } else {
+                elm["on" + type] = fn;
+            }
+        },
+        removeHandle: function (elm, type, fn) {
+            if (elm.removeEventListener) {
+                elm.removeEventListener(type, fn, false);
+            }
+            else if (el.detachEvent) {
+                elm.detachEvent("on" + type, fn);
+            } else {
+                elm["on" + type] = null;
+            }
+        },
+        getEvent: function (evnet) {
+            return evnet ? evnet : window.evnet;
+        },
+        getTarget: function (event) {
+            return event.target || event.srcElement;
+        }
+    });
+
+    window.MWood = MWood;
+
+})(this);
+
+window.onload = function () {
+    var dragDrop = function () {
+        var dragInfo = {};   //存放有关拖拽对象的一些信息
+        function handleEvent(event) {
+            e = MWood.getEvent(event);  //获得事件
+            var target = MWood.getTarget(event); //获得事件对象
+            switch (event.type) {   //判断事件类型
+                case "mousedown":
+                    if (target.className.indexOf("droggle") > -1) {
+                        dragInfo.dObj = target;   //存放事件对象
+                        var tlwh = getObjtlwh(target); //获得对象的 offsetTop,offsetLeft,offsetWidth,offsetHeight
+                        //计算出鼠标的坐标与对象offsetTop,offsetLeft的差值以便于鼠标移动时实时定位拖拽对象的位置
+                        target.x = e.clientX - tlwh[1];
+                        target.y = e.clientY - tlwh[0];
+
+                        //修改对象的position以便可以设置left和top进行拖拽 从这里开始此对象已经脱离文档流
+                        //何为文档流 当怎么样设置position或别的设置会脱离文档流请百度
+                        target.style.position = "absolute";
+
+                        //设置拖拽对象的left top width height
+                        target.style.left = (e.clientX - target.x) + "px";
+                        target.style.top = (e.clientY - target.y) + "px";
+                        target.style.width = tlwh[2] + "px";
+                        target.style.height = tlwh[3] + "px";
+
+                        //建立一个新对象填补被拖拽对象的位置，这样页面就还会按现在的排版，不会有任何更改。
+                        //如果没创建，被拖拽对象的位置就会被文档流上的其他对象占用，文章最后提供代码，可以注释掉下面4句试下两种情况
+                        dragInfo.vObj = document.createElement("div");
+                        dragInfo.vObj.style.width = target.style.width;
+                        dragInfo.vObj.style.height = target.style.height;
+                        target.parentNode.insertBefore(dragInfo.vObj, target);
+                    }
+                    break;
+                case "mousemove":
+                    if (dragInfo.dObj) {  //当鼠标移动的时候判断时候已经有对象存在（对象会在mousedown的时候存放进这个变量里）
+                        //设置拖拽对象的left和top改变位置，因为position已经在mousedown的时候改变为absolute了（鼠标坐标-保存的差值）
+                        dragInfo.dObj.style.left = (e.clientX - dragInfo.dObj.x) + "px";
+                        dragInfo.dObj.style.top = (e.clientY - dragInfo.dObj.y) + "px";
+
+                        //获取class为droggle的一组HTMl对象.Array.prototype.slice.call是给一个伪数组转为真正的数组用的,MWood(".droggle")就是取得class为droggle的一组对象
+                        var arr = Array.prototype.slice.call(MWood(".droggle"), 0);
+
+                        //循环对象
+                        for (var i = 0; i < arr.length; i++) {
+                            if (arr[i] === dragInfo.dObj)  //过滤拖拽对象
+                                continue;
+                            var tlwh = getObjtlwh(arr[i]); //获得对象的 offsetTop,offsetLeft,offsetWidth,offsetHeight
+
+                            //下面就主要是判断鼠标的位置是否为引起页面上各HTML元素在页面上位置的替换
+                            //判断鼠标x > 对比对象offsetLeft && x < （对比对象的offsetWidth + 对象对象offsetLef）。 鼠标的坐标y同理鼠标坐标x的判断
+                            //判断位置变换也可以按自己的标准来，我百度的时候看到例子里是这样判断我就直接像他那样写了
+                            if (e.x > tlwh[1] && e.x < (tlwh[1] + tlwh[2]) && e.y > tlwh[0] && e.y < (tlwh[0] + tlwh[3])) {
+                                if (e.y < ((tlwh[0] + tlwh[3]) / 2)) {
+                                    arr[i].parentNode.insertBefore(dragInfo.vObj, arr[i]);
+                                    break;
+                                }
+                                else {
+                                    if (!arr[i].nextSibling) {
+                                        arr[i].parentNode.appendChild(dragInfo.vObj);
+                                        break;
+                                    }
+                                    else {
+                                        arr[i].parentNode.insertBefore(dragInfo.vObj, arr[i].nextSibling);
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    break;
+                case "mouseup":
+                    if (dragInfo.dObj) {
+                        //把拖拽对象重新弄进文档流（static就是默认的值），替换临时占位的对象，并初始化dragInfo；
+                        dragInfo.dObj.style.position = "static";
+                        dragInfo.vObj.parentNode.insertBefore(dragInfo.dObj, dragInfo.vObj);
+                        dragInfo.dObj.parentNode.removeChild(dragInfo.vObj);
+                        dragInfo = {};
+                    }
+                    break;
+            }
+        };
+
+        //绑定事件
+        MWood.addHandle(document, "mousedown", handleEvent);
+        MWood.addHandle(document, "mousemove", handleEvent);
+        MWood.addHandle(document, "mouseup", handleEvent);
+    } ();
+};
+
+function getObjtlwh(o) {
+    var arr = [];
+    arr[0] = o.offsetTop;
+    arr[1] = o.offsetLeft;
+    arr[2] = o.offsetWidth;
+    arr[3] = o.offsetHeight;
+    return arr
+}
+
